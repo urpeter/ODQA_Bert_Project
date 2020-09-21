685b351fef5342d664371a478ff86ace822121d1
urs <36850433+urpeter@users.noreply.github.com>
2020-09-21 14:03:34 +0200

    added the Train_Bert.py and a ODQA class

---
+++ model/Train_Bert.py
@@ -0,0 +1,42 @@
+from model.ODQA_Dataset import ODQA_Dataset
+from transformers import AutoModelForQuestionAnswering
+from torch.utils.data import DataLoader
+from transformers import AdamW
+import utils.preprocessing
+import os
+import torch
+
+
+def main(train_encodings,val_encodings):
+    model = AutoModelForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad',
+                                                          return_dict=True)
+    train_dataset = ODQA_Dataset(train_encodings)
+    val_dataset = ODQA_Dataset(val_encodings)
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    model.to(device)
+    model.train()
+
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+    optim = AdamW(model.parameters(), lr=5e-5)
+
+    for epoch in range(3):
+        for batch in train_loader:
+            optim.zero_grad()
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            start_positions = batch['start_positions'].to(device)
+            end_positions = batch['end_positions'].to(device)
+            outputs = model(input_ids, attention_mask=attention_mask, start_positions=start_positions,
+                            end_positions=end_positions)
+            loss = outputs[0]
+            loss.backward()
+            optim.step()
+
+    model.eval()
+
+
+if __name__ == '__main__':
+    main()
