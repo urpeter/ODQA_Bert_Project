32c74aeadcb95ecaf77549b5f7aaf9d0c88aff81
desastrae <anastasiaborisenkov@gmail.com>
2020-09-23 12:59:13 +0200

    lowercased context to find answer-string in context

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -32,7 +32,7 @@ def add_end_idx(answ_cont_dict):
         for c in context:
             print(c, answer)
 
-            index = [(m.start(0), m.end(0)) for m in re.finditer(answer, c)]
+            index = [(m.start(0), m.end(0)) for m in re.finditer(answer, c.lowercase())]
 
             print(index)
             if index == []:
