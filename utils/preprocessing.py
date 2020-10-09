89ece42839627bfba491d37d9e4dfe075754735f
desastrae <anastasiaborisenkov@gmail.com>
2020-10-09 13:55:03 +0200

    misc

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -129,7 +129,7 @@ def process_searchqa(folder, set_type): # TODO: check if data is properly proces
                     # if len(batches_data) % 1000 == 0:
                     print("\n length batches_data " + str(len(batches_data)) + " " + str(counter))
 
-                if len(batches_data) == 3000:
+                if len(batches_data) == 3000000:
                     counter += 1
                     # def save_to_file(path, question_dic, type, set_type, doc_size=None):
                     save_batch_files("/local/anasbori/bert_odqa/ODQA_Bert_Project/batch_output", batches_data,
