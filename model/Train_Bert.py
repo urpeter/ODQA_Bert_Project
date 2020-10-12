47a0b30862b18d5bdda39c6402cdf764a9b7fbb9
ugrivous <36850433+ugrivous@users.noreply.github.com>
2020-10-12 13:46:17 +0200

    preprocessing.py changes

--- model/Train_Bert.py
+++ model/Train_Bert.py
@@ -13,7 +13,7 @@ import pickle
 import wandb
 wandb.login(key="06447813c3501a681da170acfe62a6f5aca4cf35")
 wandb.init(project="Bert_ODQA")
-
+# - *- coding: utf- 8 - *-
 
 # Returns the model
 def training():
