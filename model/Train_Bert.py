109f7098fb319944c90be8addaf9f025f426cfdf
ugrivous <36850433+ugrivous@users.noreply.github.com>
2020-10-12 15:32:23 +0200

    preprocessing.py changes

--- model/Train_Bert.py
+++ model/Train_Bert.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from pathlib import Path
 from model.ODQA_Dataset import ODQA_Dataset
 from transformers import AutoModelForQuestionAnswering, Trainer, TrainingArguments
