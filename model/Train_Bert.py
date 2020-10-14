264bca6cda9d3367078f84ac11df6c316a5fc4f8
ugrivous <36850433+ugrivous@users.noreply.github.com>
2020-10-14 14:18:52 +0200

    working on Train_Bert.py

--- model/Train_Bert.py
+++ model/Train_Bert.py
@@ -50,7 +50,7 @@ def training():
     training_args = TrainingArguments(
         output_dir='./training_output',  # output directory
         num_train_epochs=3,  # total number of training epochs
-        per_device_train_batch_size=30,  # batch size per device during training
+        per_device_train_batch_size=1,  # batch size per device during training
         per_device_eval_batch_size=30,  # batch size for evaluation
         warmup_steps=500,  # number of warmup steps for learning rate scheduler
         weight_decay=0.01,  # strength of weight decay
