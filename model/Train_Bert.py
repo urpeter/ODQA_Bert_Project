0ff5819d2f9ae9c028f6bdc62a9dc6062d6cd3e2
ugriv <ursaffeldt@gmail.com>
2020-10-22 02:22:28 +0200

    updated Train_Bert.py: save model to file, train with all files from val,dev and train

--- model/Train_Bert.py
+++ model/Train_Bert.py
@@ -11,6 +11,7 @@ import os
 #from argparse import ArgumentParser
 import csv
 import pickle
+from pathlib import Path
 # Initialize wandb for logging
 #import wandb
 #wandb.login(key="06447813c3501a681da170acfe62a6f5aca4cf35")
@@ -22,12 +23,12 @@ class ODQA_Dataset(torch.utils.data.Dataset):
         self.encodings = encodings2
 
     def __getitem__(self, idx):
-        print(idx)
-        print("Lange des Encodingdicts",len(self.encodings))
-        blub = (self.encodings.items())
+       # print(idx)
+        #print("Lange des Encodingdicts",len(self.encodings))
+        #blub = (self.encodings.items())
         #print("blub:",blub)
-        for key,val in blub:
-            print("Key,val length",key,len(val))
+        #for key,val in blub:
+         #print("Key,val length",key,len(val))
         #bla = csv.writer(open("output.csv", "w",encoding="utf-8"),delimiter=';')
         #bla.writerows(self.encodings.items())
         return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()} #<----------
@@ -38,7 +39,7 @@ class ODQA_Dataset(torch.utils.data.Dataset):
 
 def training():
     # Input paths
-    dirpath = "../batch_output"  # args.out
+    #dirpath = "../batch_output"  # args.out
 
    #DATASET_PATH_SEARCHQA = Path("/".join([dirpath, 'searchqa_train.pkl']))
     #SEARCHQA_VAL = Path("/".join([dirpath, 'searchqa_val.pkl']))
@@ -46,52 +47,64 @@ def training():
     #DATASET_PATH_QUASAR = Path("/".join([dirpath, 'quasar_train_short.pkl']))
     #QUASAR_TEST = Path("/".join([dirpath, 'quasar_test_short.pkl']))
     #QUASAR_DEV = Path("/".join([dirpath, 'quasar_dev_short.pkl']))
+    batches = {}
+    #trainset_path = os.listdir(Path("./batch_output/train"))
+    #devset_path = os.listdir(Path("./batch_output/dev"))
+    #valset_path = os.listdir(Path("./batch_output/val"))
+    paths = ["train","val","dev"]
+    for path in paths:
+        type_path = "/".join(["./batch_output",path])
+        batches[type_path] = (os.listdir(type_path))
 
-    batches = os.listdir("./batch_output/train")
     #test_set_list = [QUASAR_DEV,QUASAR_TEST,SEARCHQA_TEST,SEARCHQA_VAL]
 
     #Init model
     model = AutoModelForQuestionAnswering.from_pretrained('distilbert-base-uncased',
                                                      return_dict=True)
-    for batch in batches:
+    for tipe, files in batches.items():
         # Open Pickled file
-        with open("/".join(["./batch_output/train", batch]), 'rb') as infile:
-            print("Loading File")
-            encodings = pickle.load(infile)
-        print("Loaded File")
+        for batch in files:
+            # Open Pickled file
+            with open("/".join([tipe, batch]), 'rb') as infile:
+                print("Loading File")
+                encodings = pickle.load(infile)
+            print("Loaded File")
 
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        print("Start Training")
-        # Train on Dataset
+            device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+            print("Start Training")
+            # Train on Dataset
 
-        model.to(device)
-        model.train()
+            model.to(device)
+            model.train()
 
-        for encoding in encodings:
+            for encoding in encodings:
 
-            train_dataset = ODQA_Dataset(encoding)
-            #train_dataset.__getitem__()
-            train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+                train_dataset = ODQA_Dataset(encoding)
+                #train_dataset.__getitem__()
+                train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 
-            optim = AdamW(model.parameters(), lr=5e-5)
+                optim = AdamW(model.parameters(), lr=5e-5)
 
-            for epoch in range(3):
-                for batch in train_loader:
-                    optim.zero_grad()
-                    input_ids = batch['input_ids'].to(device)
-                    attention_mask = batch['attention_mask'].to(device)
-                    start_positions = batch['start_positions'].to(device)
-                    end_positions = batch['end_positions'].to(device)
-                    outputs = model(input_ids, attention_mask=attention_mask, start_positions=start_positions,
-                                    end_positions=end_positions)
-                    loss = outputs[0]
-                    # wandb.log({'training loss (extraction)': loss})
-                    loss.backward()
-                    optim.step()
+                for epoch in range(3):
+                    for batch in train_loader:
+                        optim.zero_grad()
+                        input_ids = batch['input_ids'].to(device)
+                        attention_mask = batch['attention_mask'].to(device)
+                        start_positions = batch['start_positions'].to(device)
+                        end_positions = batch['end_positions'].to(device)
+                        outputs = model(input_ids, attention_mask=attention_mask, start_positions=start_positions,
+                                        end_positions=end_positions)
+                        loss = outputs[0]
+                        # wandb.log({'training loss (extraction)': loss})
+                        loss.backward()
+                        optim.step()
 
-            # model.eval()
-            print("Training Batch Done")
-        print("Training for File Done")
+                # model.eval()
+                print("Training Batch Done")
+        print("Training for "+str(tipe)+ " Done")
+    print("saving model to file")
+    model.save_pretrained("./saved_model")
+    print("saved model")
 
     '''training_args = TrainingArguments(
         output_dir='./training_output',  # output directory
