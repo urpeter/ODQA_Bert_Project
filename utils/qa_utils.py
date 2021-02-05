bfe42e71a7bb612d9c9ca8ffe93bf819a068a8bc
ugriv <ursaffeldt@gmail.com>
2021-02-05 12:23:10 +0100

    id to qas_id

--- utils/qa_utils.py
+++ utils/qa_utils.py
@@ -80,6 +80,7 @@ def postprocess_qa_predictions(
     # Build a map example to its corresponding features.
     example_id_to_index = {k: i for i, k in enumerate(examples["id"])}
     features_per_example = collections.defaultdict(list)
+
     for i, feature in enumerate(features):
         features_per_example[example_id_to_index[feature["example_id"]]].append(i)
 
